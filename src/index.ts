1b6d387b3caeb917416577ea31a2b70b567c1f99
atsfour <atsfour@users.noreply.github.com>
2023-12-26 15:38:38 +0900

    separate schema.graphql

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,22 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadSchemaSync } from "@graphql-tools/load";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const typeDefs = loadSchemaSync("./schema.graphql", {
+  loaders: [new GraphQLFileLoader()],
+});
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
+const data = [
   {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
+    id: "person1",
+    name: "John Doe",
+    friends: [],
   },
   {
-    title: 'City of Glass',
-    author: 'Paul Auster',
+    id: "person2",
+    name: "Nanashi",
+    friends: [],
   },
 ];
 
@@ -36,7 +24,7 @@ const books = [
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    persons: () => data,
   },
 };
 
